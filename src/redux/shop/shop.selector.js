696be99c64acc8b481c1a815aa390f20ebe30562
Erdem Alpay <erdemalpay@gmail.com>
2020-11-22 18:01:48 +0300

    Add shop data fetch and withSpinner

--- src/redux/shop/shop.selector.js
+++ src/redux/shop/shop.selector.js
@@ -10,11 +10,11 @@ export const selectShopCollections = createSelector(
 export const selectCollection = collectionUrlParam => (
   createSelector(
     [selectShopCollections],
-    collections => collections[collectionUrlParam]
+    collections => collections ? collections[collectionUrlParam] : null
   )
 )
 
 export const selectCollectionForPreview = createSelector(
   [selectShopCollections],
-  collections => Object.keys(collections).map(key => collections[key])
+  collections => collections ? Object.keys(collections).map(key => collections[key]) : []
 )
